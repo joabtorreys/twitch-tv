96950f329b2dbcf13b4360311a47b0c8ffaf9e24
iamjoey <j.torreys@outlook.com>
2018-03-16 19:52:35 -0400

    this link to this api is shit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+// running jquery.
+$(document).ready(function () {
+// get freecodecamp status
+var clientid = "hfh578b0sqfosmvev5nth7hpkdikec";
+var url = "https://wind-bow.glitch.me/twitch-api/streams/:stream";
+$.getJSON(url,function (data1) {
+
+console.log(data1);
+
+
+});
+
+
+
+});
